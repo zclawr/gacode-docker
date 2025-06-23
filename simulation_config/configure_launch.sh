f2c3c7c53ec93badcca2fb6395b8bd0eb7fe4f45
zclawr <zclawrence@ucsd.edu>
2025-06-23 12:24:06 -0700

    working s3 and local build, pushing before refactoring Dockerfile with conda, poetry

--- simulation_config/configure_launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp ./launch_template.yml ./launch.yml
-echo "inputs:
-  default:
-    hparam:
-      _s3path: $2
-run:
-  simulation: [$1]
-  inputs: [default]" >> ./launch.yml
--- src/run_container_simulation.sh
+++ src/run_container_simulation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-docker create --name gacode-sim-tmp gacode-docker
-docker cp $2 gacode-sim-tmp:/home/user/sim
-docker commit gacode-sim-tmp gacode-sim
-docker run gacode-sim $1
+# $1 : first param is simulation type [tglf, cgyro]
+# $2 : second param is the path to input files
+# docker pull zclawr/ai-fusion
+# docker create --name gacode-sim-tmp gacode-tglf-cgyro
+# docker cp $2 gacode-sim-tmp:/home/user/sim
+# docker commit gacode-sim-tmp gacode-sim
+# docker run gacode-sim $1
+echo "HELLO THERE"
